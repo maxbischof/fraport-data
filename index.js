81c0de0f62ecced828a3552c3d3bfe4dc592367c
Max <max@bischof.dk>
2020-10-21 10:26:44 +0200

    Get arrivals from fraport api

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import { writeDocument } from './lib/firestore'
+const firestore = require('./lib/firestore.js')
+const fraport = require('./lib/fraport.js')
 
 const arrivalObject = {
   id: 22,
   start: '20.09.20',
 }
 
-writeDocument({
+firestore.writeDocument({
   collection: 'arrival',
   identifier: '20',
   document: arrivalObject,
 })
+
+fraport.getArrivals()

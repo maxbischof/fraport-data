5ce35aebbe46f1eb9f763ea6c6913b183315af7f
Max <max@bischof.dk>
2020-10-21 16:53:16 +0200

    Remov node cron package and make get function async

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const firestore = require('./lib/firestore')
 const fraport = require('./lib/fraport')
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get('/cron/getflights', (req, res) => {
-    const arrivals = await fraport.getFlights('arrival')
-    arrivals.forEach((arrival) => {
-      firestore.writeDocument({
-        collection: 'arrivals',
-        identifier: arrival.flight.id,
-        document: arrival.flight,
-      })
+app.get('/cron/getflights', async (req, res) => {
+  const arrivals = await fraport.getFlights('arrival')
+  arrivals.forEach((arrival) => {
+    firestore.writeDocument({
+      collection: 'arrivals',
+      identifier: arrival.flight.id,
+      document: arrival.flight,
     })
-  
-    const departures = await fraport.getFlights('departure')
-    departures.forEach((departure) => {
-      firestore.writeDocument({
-        collection: 'departures',
-        identifier: departure.flight.id,
-        document: departure.flight,
-      })
+  })
+
+  const departures = await fraport.getFlights('departure')
+  departures.forEach((departure) => {
+    firestore.writeDocument({
+      collection: 'departures',
+      identifier: departure.flight.id,
+      document: departure.flight,
     })
-});
+  })
+})
 
 const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
+  const host = server.address().address
+  const port = server.address().port
 
-  console.log(`Example app listening at http://${host}:${port}`);
-});
-
-
-  
+  console.log(`Example app listening at http://${host}:${port}`)
+})

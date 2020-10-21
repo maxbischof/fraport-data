eec57e9197450e4afc2adff62bdae73b72ef2366
Max <max@bischof.dk>
2020-10-21 09:42:37 +0200

    Move writeDocument as function to lib folder

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const firebase = require('firebase')
-require('dotenv').config()
-
-const apiKey = process.env.FIREBASE_API_KEY
-const authDomain = process.env.FIREBASE_AUTH_DOMAIN
-const projectID = process.env.FIREBASE_PROJECT_ID
-
-firebase.initializeApp({
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectID,
-})
-
-const db = firebase.firestore()
+import { writeDocument } from './lib/firestore'
 
 const arrivalObject = {
   id: 22,
   start: '20.09.20',
 }
 
-db.collection('arrivals')
-  .doc('23')
-  .set(arrivalObject)
-  .then(() => {
-    console.log('Document successfully written!')
-  })
-  .catch((error) => {
-    console.error('Error writing document: ', error)
-  })
+writeDocument({
+  collection: 'arrival',
+  identifier: '20',
+  document: arrivalObject,
+})
